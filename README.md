d7a86931d1cc2ddcc6380ba9e523a53e8f09803e
Anna-Myzukina <anna.muzykina83@gmail.com>
2021-07-22 07:04:13 +0300

    Add function to get dataand connect api

--- README.md
+++ README.md
@@ -17,4 +17,8 @@ add next line of code
     
 ![img](https://github.com/Anna-Myzukina/weather_radar/blob/main/images/screen1.PNG)
 
+- [Create Real Flutter Splash Screen - Flutter Native Splash Package - Android, iOS, Web 2021](https://www.youtube.com/watch?v=itpd898uhyg)
+
+        $ flutter pub add flutter_native_splash
+
 
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:lottie/lottie.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_radar/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -10,14 +11,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    print('this line of code is triggered');
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  // void getData() async {
+  //   http.Response response = await http.get(Uri.parse(
+  //       'api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=97afc688398e7069addf4cc712e23535'));
+
+  //   print(response.body);
+  // }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=97afc688398e7069addf4cc712e23535'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -25,34 +53,44 @@ class _LoadingScreenState extends State<LoadingScreen> {
             begin: Alignment.topRight,
             end: Alignment.bottomRight,
             colors: [
+              Color(0xFFFFBF00),
               Color(0xFFFF8008),
               Color(0xFFCE1010),
             ],
           ),
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            SizedBox(
-              // height: 300,
-              // width: 300,
-              child: ElevatedButton(
-                onPressed: () {
-                  getLocation();
-                },
-                child: Text('Геолокация'),
-              ),
+            // SizedBox(
+            //   // height: 300,
+            //   // width: 300,
+            //   child: ElevatedButton(
+            //     onPressed: () {
+            //       getLocation();
+            //     },
+            //     child: Text('Геолокация'),
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 50,
+            // ),
+            Center(
+              child: SizedBox(
+                  height: 400,
+                  width: 400,
+                  child: Lottie.asset("lottie/earth.json")),
             ),
-            SizedBox(
-              height: 50,
-            ),
+            // SizedBox(
+            //   height: 50,
+            // ),
             Center(
               child: SizedBox(
                   height: 300,
                   width: 300,
                   child: Lottie.asset("lottie/circle.json")),
-            )
+            ),
           ],
         ),
       ),
